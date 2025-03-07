0b10155e0e9364585e2618d5d03be9a4895954ba
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2025-03-07 15:19:31 +0200

    Changed router to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import SignIn from './SignIn'
 import SignUp from './SignUp'
 
 function App() {
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<SignIn />} />
                 <Route path="/signUp" element={<SignUp />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
